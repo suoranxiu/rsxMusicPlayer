bf094bd259dfbf2c480f1e5bc1eebe36a8288998
srx <542663467@qq.com>
2019-10-15 22:08:07 +0800

    手机本地音乐列表获取

--- app/src/main/java/com/example/testmusicplayer/pager/ListPager.java
+++ app/src/main/java/com/example/testmusicplayer/pager/ListPager.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.testmusicplayer.R;
 import com.example.testmusicplayer.base.BasePager;
 
 /**
@@ -13,6 +16,8 @@ import com.example.testmusicplayer.base.BasePager;
  */
 public class ListPager extends BasePager {
 
+    private ListView listView;
+    private ProgressBar progressBar;
     private TextView textView;
 
     public ListPager(Context context) {
@@ -21,16 +26,25 @@ public class ListPager extends BasePager {
 
     @Override
     public View initView() {
-        Log.e("ListPager","listPage is initialized");
-        textView = new TextView(context);
-        textView.setTextSize(25);
-        textView.setGravity(Gravity.CENTER);
-        return textView;
+        View view = View.inflate(context, R.layout.list_pager,null);
+        listView = (ListView)view.findViewById(R.id.lv_lp_music_list);
+        textView = (TextView)view.findViewById(R.id.tv_noMusic);
+        progressBar = (ProgressBar)view.findViewById(R.id.pb_loading);
+        return view;
     }
 
     @Override
     public void initDate() {
-        Log.e("ListPager","listPage data is initialized");
-        textView.setText("music");
+        //加载数据
+        getDataFromLocal();
+    }
+
+    /**
+     * 从本地的SD卡读取数据
+     * 1.根据后缀名遍历SDcard（用不多）
+     * 2.去内容提供者获取音乐文件
+     */
+    private void getDataFromLocal() {
+
     }
 }
